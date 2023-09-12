ac8af6cc6506d85821ffe889ddb548be931c29f6
alexanderalmada <alexander.d.almada@gmail.com>
2023-09-11 18:13:04 -0700

    App and Main commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
+  const [token, setToken] = useState(null);
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <SignUpForm token={token} setToken={setToken} /> */}
+      <Authenticate token={token} setToken={setToken} />
+      <Routes>
+        <Route
+          path="/"
+          element={<SignUpForm token={token} setToken={setToken} />}
+        />
+        {/* <Route path="/red" element={<Red />} /> */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </>
   );
 }
 
-export default App;
+// const options = {
+//   method: "GET",
+//   url: "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming",
+//   headers: {
+//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+//     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await axios.request(options);
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
